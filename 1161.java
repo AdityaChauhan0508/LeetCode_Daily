70b864356485bb3fa56ee26e001ce20308a9fa1b
Aditya Chauhan <officialemail032@gmail.com>
2026-01-06 23:36:54 +0530

    Maximum Level Sum of a Binary Tree

--- 1161.java
+++ 1161.java
@@ -1,6 +1,21 @@
 import java.util.*;
 
 class Solution {
+    
+ //Definition for a binary tree node.
+  public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     
+     
+    //  TreeNode(int val, TreeNode left, TreeNode right) {
+    //      this.val = val;
+    //     this.left = left;
+    //     this.right = right;
+    // }
+ }
+ 
     public int maxLevelSum(TreeNode root) {
         if (root == null) return 0;
 
