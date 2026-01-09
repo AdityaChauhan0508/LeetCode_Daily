698cd1d8ad521fa13fdd912a3951fad5dba820a4
Aditya Chauhan <officialemail032@gmail.com>
2026-01-09 22:40:04 +0530

    smallest subtree

--- 865.java
+++ 865.java
@@ -4,16 +4,16 @@
 
 class Solution {
     public class TreeNode {
-      int val;
+      //int val;
       TreeNode left;
       TreeNode right;
       //TreeNode() {}
       //TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
+    //   TreeNode(int val, TreeNode left, TreeNode right) {
+    //       this.val = val;
+    //       this.left = left;
+    //       this.right = right;
+    //   }
   }
     private class NodeHelper {  
         TreeNode node;
