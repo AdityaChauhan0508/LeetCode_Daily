fe8e2f32f18d03b5e09a3e524ac0d9a481b5b84b
Aditya Chauhan <officialemail032@gmail.com>
2026-05-04 22:58:00 +0530

    Refactor matrix rotation method to use transpose and reverse approach

--- 48.java
+++ 48.java
@@ -1,13 +1,40 @@
+// class Solution {
+//     public void rotate(int[][] mat) {
+//         int n = mat.length, k = n - 1;
+//         for (int i = 0; i < n >> 1; i++)
+//             for (int j = i; j < k - i; j++) {
+//                 int t = mat[i][j];
+//                 mat[i][j] = mat[k - j][i];
+//                 mat[k - j][i] = mat[k - i][k - j];
+//                 mat[k - i][k - j] = mat[j][k - i];
+//                 mat[j][k - i] = t;
+//             }
+//     }
+// }
+
 class Solution {
-    public void rotate(int[][] mat) {
-        int n = mat.length, k = n - 1;
-        for (int i = 0; i < n >> 1; i++)
-            for (int j = i; j < k - i; j++) {
-                int t = mat[i][j];
-                mat[i][j] = mat[k - j][i];
-                mat[k - j][i] = mat[k - i][k - j];
-                mat[k - i][k - j] = mat[j][k - i];
-                mat[j][k - i] = t;
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
+        }
+
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
     }
 }
